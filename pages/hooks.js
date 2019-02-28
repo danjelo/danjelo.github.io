605815ecdee09675da1c8626ccc41a24a5dbf9de
danjelo <daniel_plyhm@hotmail.com>
2019-02-28 11:02:10 +0100

    Create hooks.js

---
+++ pages/hooks.js
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <title>DP's CODE NOTES</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="style.css" rel="stylesheet" />
+    <link href="prism.css" rel="stylesheet" />
+    <link href='https://fonts.googleapis.com/css?family=Share+Tech+Mono&effect=destruction' rel='stylesheet' type='text/css' />
+    <script defer src="https://use.fontawesome.com/releases/v5.0.4/js/all.js"></script>
+</head>
+
+<body>
+
+    
+    <header>
+            <a href="../index.html">
+                <h1 class="font-effect-destruction">
+                    <i class="fas fa-code-branch"></i>
+                    DP's CODE NOTES
+                    <i class="fas fa-code"></i>
+                </h1>
+            </a>        
+
+      </header>
+<main>
+
+<h2>React hook </h2>
+    <span class="pubdate">2019-02-27</span>
+    <p></p>
+ 
+<script src="https://gist.github.com/danjelo/623552cc1f1fba3c5ef41a403db29c9c.js"></script>
+</main>
+
+
+
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- danjelo -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-4174842744027387"
+     data-ad-slot="7693952539"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+</body>
+
+</html>
